8cd7ac49fed5c360ece0ffb78f1a991fa7921d3a
Fonroques <fonroquest@gmail.com>
2020-12-16 20:31:27 +0100

    update test.yml

--- index.ts
+++ index.ts
@@ -16,7 +16,7 @@ require("./init/routes")(app);
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () =>
-  console.log(`Listening on port ${port}...${dbName}...${password}`)
+  console.log(`Listening on port ${port}...`)
 );
 
 module.exports = server;
